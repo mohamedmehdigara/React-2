9b1fcbfd9e97543ee80f7d0b58531d689e811c98
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2022-02-04 17:24:06 +0100

    Added files

---
+++ week1/practice-exercises/1-extreme-hobbies/extreme-hobbies/src/components/HobbyList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HobbyList() {
+    const hobbies = ["Surfing", "Rock climbing", "Mountain biking", "Breakdancing"];
+
+  return <div>
+  </div>;
+}
+
+export default HobbyList;
+

30147fdd6137a8bd215802a85dda754f1f81175c
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2022-02-05 11:43:30 +0100

    Added files

--- week1/practice-exercises/1-extreme-hobbies/extreme-hobbies/src/components/HobbyList.js
+++ week1/practice-exercises/1-extreme-hobbies/extreme-hobbies/src/components/HobbyList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 function HobbyList() {
-    const hobbies = ["Surfing", "Rock climbing", "Mountain biking", "Breakdancing"];
-
-  return <div>
-  </div>;
+  const hobbies = ["Surfing", "Rock climbing", "Mountain biking", "Breakdancing"];
+  const hobby = props.hobbies;
+  const listItems = hobby.map((Hobby) =>
+    <li>{Hobby}</li>
+  );
+  return( <div>
+    {listItems} key={i}
+  </div>);
 }
 
 export default HobbyList;
